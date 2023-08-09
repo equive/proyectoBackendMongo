3131cd0c6d25116388cb010f94c5937b89c62669
Saul E.V <equive@gmail.com>
2023-08-08 21:19:37 -0600

    Mi primer commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const dotenv = require("dotenv").config()
+const port = process.env.PORT || 5000
+
+const app = express()
+app.get('/api/tareas', (req, res)=>{
+    res.status(200).json({message : 'Obtener tareas'})
+})
+
+app.listen(port, ()=>{
+    console.log(`Server startert on port ${port}`)
+})
