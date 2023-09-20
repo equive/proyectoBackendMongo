760e32c8e23eb5f5626890b198fb00e89cc41c2c
Saul E.V <equive@gmail.com>
2023-09-19 19:45:14 -0600

    instalacion cors

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,12 @@ const dotenv = require("dotenv").config()
 const port = process.env.PORT || 5000
 const errorHandler = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const cors = require('cors')
 
 connectDB()
 const app = express()
+
+app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 app.use(errorHandler)
